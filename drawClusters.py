97c3e9eef666c37b60fdcffab36bce2bc72a2af4
Christopher Pyatt (QIB) <christopher.pyatt@quadram.ac.uk>
2019-07-11 14:39:46 +0100

    Increase image res + better font

--- drawClusters.py
+++ drawClusters.py
@@ -10,6 +10,12 @@ import colorsys
 
 #im = Image.new("RGBA", (800,200), (255,255,255,255))
 
+'''
+This script takes a tab-delimited file produced by findClusters.py. The drawMultipleClusters() function reads the input file and creates a canvas on which to draw the gene clusters specified in the input file. It then calls drawCluster() to draw each gene cluster, one underneath the other. drawCluster() itself calls the drawArrow() method to draw each arrow (representing each gene) onto a black line representing the DNA strand.
+
+Font used in image is 'Academic M54', downloaded from https://www.wfonts.com/font/academic-m54. It is licensed as 'Free for personal use', covering this script since it is not exploited for profit. No copyright infringement intended.
+'''
+
 parser = argparse.ArgumentParser(description='Input a clusters file. Output a drawing of those clusters.')
 parser.add_argument('inputFile',
                     help='Clusters file created by findClusters.py.')
@@ -25,12 +31,13 @@ omitStrains = args.omitStrains
 
 def drawArrow(im, direction, colour, position, label, length):
     draw = ImageDraw.Draw(im)
+    fnt = ImageFont.truetype('AcademicM54.ttf', 45)
     if direction == "+":
-        draw.polygon([position, (position[0]+length,position[1]), (position[0]+length,position[1]-15), (position[0]+length+25,position[1]+10), (position[0]+length,position[1]+35), (position[0]+length,position[1]+20), (position[0],position[1]+20)], fill=colour)
-        ImageDraw.Draw(im).text((position[0]+length/3,position[1]+40), label, fill="black")
+        draw.polygon([position, (position[0]+length,position[1]), (position[0]+length,position[1]-60), (position[0]+length+100,position[1]+40), (position[0]+length,position[1]+140), (position[0]+length,position[1]+80), (position[0],position[1]+80)], fill=colour)
+        ImageDraw.Draw(im).text((position[0]+length/3,position[1]+160), label, font=fnt, fill="black")
     elif direction == "-":
-        draw.polygon([(position[0],position[1]+10), (position[0]+25,position[1]-15), (position[0]+25,position[1]), (position[0]+25+length,position[1]), (position[0]+25+length,position[1]+20), (position[0]+25,position[1]+20), (position[0]+25,position[1]+35)], fill=colour)
-        ImageDraw.Draw(im).text((position[0]+length/3,position[1]+40), label, fill="black")
+        draw.polygon([(position[0],position[1]+40), (position[0]+100,position[1]-60), (position[0]+100,position[1]), (position[0]+100+length,position[1]), (position[0]+100+length,position[1]+80), (position[0]+100,position[1]+80), (position[0]+100,position[1]+140)], fill=colour)
+        ImageDraw.Draw(im).text((position[0]+length/3,position[1]+160), label, font=fnt, fill="black")
     else: print("Invalid direction: Specify either \"+\" or \"-\".")
     del draw
 
@@ -61,9 +68,9 @@ def drawCluster(im, strain, genes, colours, position):
     #im2 = Image.resize((100*len(genes)+100,200))
     sorted_genes = sorted(genes.strip().split(" "), key=lambda x: x.split(":")[1])
     sameContig = [list(v) for k, v in itertools.groupby(sorted_genes, lambda y: y.split(":")[1])]
-    groupPos = 125
-    fnt = ImageFont.truetype('FreeMono.ttf', 15)
-    ImageDraw.Draw(im).text((20,100*position-5), strain, font=fnt, fill="black")
+    groupPos = 500
+    fnt = ImageFont.truetype('AcademicM54.ttf', 60)
+    ImageDraw.Draw(im).text((80,400*position-20), strain, font=fnt, fill="black")
     for group in sameContig:
         sorted_group = sorted(group, key=lambda x: int(x.split(":")[3]))
         groupL = 0
@@ -81,10 +88,10 @@ def drawCluster(im, strain, genes, colours, position):
             groupL += int(splt[5]) + gap
         group_start = int(sorted_group[0].split(":")[3])
         group_end = int(sorted_group[-1].split(":")[4])
-        group_length = groupL/100+50
+        group_length = groupL/25+200
         #ImageDraw.Draw(im).line((groupPos, 100*position, 100*len(group)+groupPos+25, 100*position), fill="black")
         #ImageDraw.Draw(im).line((groupPos, 100*position, group_length+groupPos+25, 100*position), fill="black")
-        ImageDraw.Draw(im).line((groupPos, 100*position, group_length+groupPos+len(group)*25, 100*position), fill="black")
+        ImageDraw.Draw(im).line((groupPos, 400*position, group_length+groupPos+len(group)*100, 400*position), fill="black", width=11)
         idx = 0
         #colours = get_N_HexCol(len(genes))
         genePos = 0
@@ -95,13 +102,13 @@ def drawCluster(im, strain, genes, colours, position):
             dir = geneSPLT[2]
             lab = geneSPLT[0]
             col = colours[lab]
-            length = int(geneSPLT[5])/100
+            length = int(geneSPLT[5])/25
             #pos = (groupPos+25+100*(idx), 100*position-10)
             if gapList[idx] > 10000:
-                genePos += prevLen +25 + 100
-                ImageDraw.Draw(im).polygon([(groupPos+genePos-75, 100*position-10), (groupPos+genePos-25, 100*position-10), (groupPos+genePos-25, 100*position+10), (groupPos+genePos-75, 100*position+10)], fill="red")
+                genePos += prevLen +100 + 400
+                ImageDraw.Draw(im).polygon([(groupPos+genePos-300, 400*position-40), (groupPos+genePos-100, 400*position-40), (groupPos+genePos-100, 400*position+40), (groupPos+genePos-300, 400*position+40)], fill="red")
                 #ImageDraw.Draw(im).line((groupPos+genePos-30, 100*position-20, groupPos+genePos-30, 100*position+20), fill="red")
-                ImageDraw.Draw(im).text((groupPos+genePos-70, 100*position-5), str(gapList[idx]), fill="black")
+                ImageDraw.Draw(im).text((groupPos+genePos-280, 400*position-20), str(gapList[idx]), fill="black")
                 #ImageDraw.Draw(im).text((groupPos+genePos-40, 100*position-30), str(int(geneSPLT[3])-prevPos), fill="black")
                 #genePos += length + 75
             #elif 0 < (int(geneSPLT[3])-group_start)/100-prevPos < 35:
@@ -109,13 +116,13 @@ def drawCluster(im, strain, genes, colours, position):
             #elif gene != sorted_group[-1]:
             #    genePos += length+gap/100 +25 +gapList[idx]/100
             else:
-                genePos += prevLen+gapList[idx]/100 +25
-            pos = (groupPos+genePos, 100*position-10)
+                genePos += prevLen+gapList[idx]/25 +100
+            pos = (groupPos+genePos, 400*position-40)
             drawArrow(im, dir, col, pos, lab, length)
             prevPos = int(geneSPLT[4])
             prevLen = length
             idx += 1
-        groupPos += group_length+len(group)*25+25
+        groupPos += group_length+len(group)*100+100
     #im.save("test_img.png", "PNG")
 
 ### old version
@@ -165,7 +172,7 @@ def drawMultipleClusters(clusters):
             listGenes[gene] = colours[idx]
             idx += 1
         #print(listGenes)
-        im = Image.new("RGBA", (140*maxGenes+100,110*numClusts+100), (255,255,255,255))
+        im = Image.new("RGBA", (560*maxGenes+400,440*numClusts+400), (255,255,255,255))
         clustIdx = 1
         file.seek(0)
         for line in file:
@@ -178,7 +185,18 @@ def drawMultipleClusters(clusters):
                 drawCluster(im, strain, cluster, listGenes, clustIdx)
                 clustIdx += 1
         outfile = outputFile + ".png"
-        im.save(outfile, "PNG")
+        outfile2 = outputFile + ".eps"
+        im.save(outfile, format = "PNG", quality = 95)
+        
+        #png = Image.open(outfile)
+        #png.load()
+        '''
+        eps = Image.new("RGB", im.size, (255, 255, 255))
+        eps.paste(im, mask=im.split()[3])
+        
+        eps.save(outfile2, format = "EPS", quality = 80)
+        '''
+        #im.save(outfile2, "PDF", save_all=True)
 
 
 drawMultipleClusters(inputFile)
